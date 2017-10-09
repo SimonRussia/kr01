dd4f5eefa2d9c1027b3e2c08f85e4bed8c019f21
SimonRussia <git.streisand@gmail.com>
2017-10-09 22:43:18 +0300

    Update task_2.cpp

--- task_2.cpp
+++ task_2.cpp
@@ -1,33 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 template <typename Iterator, typename T>
 auto find_two_elements_with_sum(Iterator first, Iterator last, T c) {
-  bool trueIterators = false;
-  int arr[10] = {0,1,2,3,4,5,6,7,8,9};
-
-  if(first == last || first > last) cout << "ERROR:\tWrong range..." << endl;
-  if(c < 1 || c > 17) cout << "ERROR:\tWrong summ value..." << endl;
-
-  while(first != last) {
-    int Summ = arr[first] + arr[last];
-    if(Summ < c)
-      first++;
-    else if(Summ > c)
-      last--;
-    else {
-      trueIterators = true;
-      cout << "Our Iterators " << first << " " << last << endl;
-      break;
+  for (; first != last; ++first) {
+    for (Iterator first_x = first; first_x != last; ++first_x) {
+      if ((*first + *first_x) == c) {
+        cout << *first << " " << *first_x << endl;
+      }
     }
   }
-  return 0;
 }
 
 int main() {
-  int sumElement = 15;
-  size_t a = 0, b = 9;
-  find_two_elements_with_sum(a, b, sumElement);
+  vector<int> v1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  find_two_elements_with_sum(v1.begin(), v1.end(), 16);
 
   return 0;
 }
